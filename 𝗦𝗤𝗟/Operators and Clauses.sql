46f19cc0976b84850da20f265eef86ea31580d16
Rohit-coder-py <rohitjha7386@gmail.com>
2026-07-06 07:37:12 +0530

    Attempt 1

---
+++ 𝗦𝗤𝗟/Operators and Clauses.sql
@@ -0,0 +1,117 @@
+-- Clauses with operators and aggregationm function 
+
+----Operators : 
+
+-- Comparison
+-- Logical
+-- Range
+-- Membership
+-- Pattern Matching
+-- Null Checking
+-- Existence (later)
+
+
+-- 1. Comparision : =, !=, <>, >, <, >=, <=
+
+
+Select * from flipkart_db;
+
+
+Select * from flipkart_db where Category = 'Electronics';
+
+
+Select * from flipkart_db where Category != 'Electronics';
+
+Select * from flipkart_db where Category <> 'Electronics';            -- <> = not equal to 
+
+Select * from flipkart_db where price_max > 999;
+
+Select COUNT(*) from flipkart_db where price_max > 999;
+
+-- Logical (AND, OR, NOT)
+
+Select * from flipkart_db where price_max > 999 and category = 'Electronics';
+
+SELECT * FROM flipkart_db WHERE category = 'Electronics' OR category = 'Furniture';
+
+SELECT * FROM flipkart_db WHERE NOT category = 'Electronics';
+--Range (BETWEEN)
+
+Select * from flipkart_db where price_max Between 1000 and 4000;
+
+
+--Membership (IN, NOT IN)
+
+Select * from flipkart_db where category in ('Electronics','Furniture','Wearables')
+
+
+Select * from flipkart_db where category NOT IN ('Electronics','Furniture','Wearables')
+
+
+--  Pattern Matching (LIKE, ILIKE,NOT LIKE)
+
+Select * from flipkart_db where product_name like 'P%';            -- P in start 
+
+Select * from flipkart_db where product_name like '%C%';           -- C in middle 
+
+-- % means start 
+-- % means end
+
+Select * from flipkart_db where product_name like '_a%';           --second character should be 'a'
+
+
+
+-----------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------
+
+
+-- Now Aggregate Functions 
+
+-- COUNT()
+-- SUM()
+-- AVG() 
+-- MIN()
+-- MAX()
+
+SELECT AGGREGATE_FUNCTION(column_name) FROM table_name WHERE condition;
+
+--COUNT()
+
+Select COUNT(product_id) from flipkart_db 
+
+Select COUNT(product_id) from flipkart_db where price_max>1000;
+
+
+--SUM()
+
+-- Only for numeric columns 
+
+Select SUM(price_max) from flipkart_db;
+
+Select SUM(price_max) from flipkart_db where category = 'Electronics';
+
+--AVG()
+
+Select AVG(price_max) from flipkart_db;
+
+Select round(AVG(price_max),2) from flipkart_db;
+
+
+
+--MIN()
+
+Select Min(price_max) from flipkart_db;
+
+Select * from flipkart_db where price_max = 99;
+
+
+--MAX()
+
+Select MAX(price_max) from flipkart_db;
+
+
+Select MAX(price_max) from flipkart_db where category = 'Electronics' or category = 'Furniture';
+
+
+
