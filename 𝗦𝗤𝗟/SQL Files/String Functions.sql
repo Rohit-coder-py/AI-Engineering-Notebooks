9e94a6f27e911f5cf061132f182e79a6b0ffe168
Rohit-coder-py <rohitjha7386@gmail.com>
2026-07-06 15:07:51 +0530

    String Function Notes

--- 𝗦𝗤𝗟/SQL Files/String Functions.sql
+++ 𝗦𝗤𝗟/SQL Files/String Functions.sql
@@ -2,3 +2,53 @@
 -- like names, categories, SKUs, etc.
 
 
+SELECT REPEAT('*', 1000);
+
+
+--  Upper() Convert text to uppercase
+
+
+Select * from flipkart_db;
+
+
+Select UPPER(product_name) from flipkart_db;
+
+
+-- LOWER()	Convert text to lowercase
+
+Select LOWER(product_name) from flipkart_db;
+
+
+-- LENGTH()	Count the number of characters
+
+Select LENGTH(product_name) from flipkart_db;
+
+
+-- TRIM()	Remove spaces from both ends
+
+Select TRIM(product_name) from flipkart_db;
+
+
+
+-- SUBSTRINGS  Extract part of a string
+
+
+Select substring('Heyy_letscodee',1,6) ;
+
+
+Select substring(product_name,1,2),skucode from flipkart_db;
+
+
+
+-- Left() and Right()  Return characters from the left and right
+
+
+
+Select left('Heyy_letscodee',5)
+
+
+Select right('Heyy_letscodee',5)
+
+
+
+
