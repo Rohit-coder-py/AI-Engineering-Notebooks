61097030aca72830b68b58ad54008eafd33f6b43
Rohit-coder-py <rohitjha7386@gmail.com>
2026-07-07 08:33:48 +0530

    Alter and case

---
+++ 𝗦𝗤𝗟/SQL Files/Alter and Case.sql
@@ -0,0 +1,13 @@
+-- USE CASES OF ALTER:
+
+-- ALTER is used to change the structure of an existing table
+
+-- 1. Add new columns
+-- 2. Remove columns
+-- 3. Rename columns
+-- 4. Change data types
+-- 5. Set or remove default values
+-- 6. Add or remove constraints
+-- 7. Rename the table
+
+
