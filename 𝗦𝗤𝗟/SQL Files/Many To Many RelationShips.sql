ff3d401e15f879b48512505127f341fa28a29d2a
Rohit-coder-py <rohitjha7386@gmail.com>
2026-07-09 12:52:10 +0530

    Added Views and Procedures

---
+++ 𝗦𝗤𝗟/SQL Files/Many To Many RelationShips.sql
@@ -0,0 +1,75 @@
+-- We used to create a junction table here 
+
+CREATE TABLE students (
+student_id INT PRIMARY KEY,
+student_name VARCHAR(100)
+);
+
+-- Sample Data
+INSERT INTO students (student_id, student_name) VALUES
+(1, 'Akarsh'),
+(2, 'Simran'),
+(3, 'Rohan');
+
+
+
+Select * from students;
+
+
+CREATE TABLE courses (
+course_id INT PRIMARY KEY,
+course_name VARCHAR(100)
+
+);
+
+-- Sample Data
+INSERT INTO courses (course_id, course_name) VALUES
+(101, 'Python'),
+(102, 'SQL'),
+(103, 'Power BI');
+
+
+--created 2 tables 
+
+CREATE TABLE student_courses (
+student_id INT,
+course_id INT,
+PRIMARY KEY (student_id, course_id),
+FOREIGN KEY (student_id) REFERENCES students(student_id),
+FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
+
+--using two primary keys together is called ; composite primary key 
+
+
+INSERT INTO student_courses (student_id, course_id) VALUES
+
+(1, 101),
+(1, 102),
+(2, 101),
+(2, 103),
+(3, 102);
+
+SELECT
+s.student_name,
+c.course_name
+FROM
+student_courses sc
+JOIN students s ON sc. student_id = s.student_id
+JOIN courses c ON sc. course_id = c.course_id;
+
+
+
+SELECT
+s.student_name,
+c.course_name
+FROM
+student_courses sc
+JOIN students s ON sc. student_id = s.student_id
+JOIN courses c ON sc. course_id = c.course_id
+where s.student_name='Simran'
+
+===================================
+Select * from students;
+Select * from courses;
+Select * from student_courses;
---
+++ 𝗦𝗤𝗟/SQL Files/Views and Procedures.sql
@@ -0,0 +1,122 @@
+A view is a virtual table based on a SQL query.
+It does not store actual data, but shows results when
+accessed - just like a saved query
+
+
+* Simplify Complex Queries
+  Save a long query and access it like a table.
+
+* Reuse Logic
+  No need to rewrite JOINs or filters again and again.
+
+* Security
+  Expose only selected columns instead of giving full access to the table.
+
+* Abstraction
+  Hide table complexity for front-end/dashboard users.
+
+* Maintainability
+  If the logic changes, update the view once and the changes reflect everywhere.
+
+
+============================================
+============================================
+
+Syntax : 
+CREATE VIEW view_name AS
+SELECT column1, column2
+FROM table_name
+WHERE condition;
+
+
+
+
+select * from flipkart_db
+
+
+CREATE VIEW expensive_products AS
+SELECT product_name, price_max
+FROM flipkart_db
+WHERE price_max > 1000;
+
+SELECT * FROM expensive_products;
+
+
+
+-- Create a views for low stock items 
+
+select * from flipkart_db
+
+
+
+Create view low_stock as 
+Select product_name , stock_quantity 
+From flipkart_db
+where stock_quantity <20;
+
+
+Select * from low_stock
+
+
+
+======================================================================================
+                               Views COMPLETED
+==========================================================================================
+
+
+
+Procedures
+
+A procedure is a block of SQL code that performs a
+series of operations - like inserting, updating, deleting,
+or selecting data - and is stored in the database.
+" Think of it like a function in programming - once
+defined, you can call it again and again without rewriting
+the logic."
+
+
+* Reusability
+
+  * Write once, use many times.
+
+* Security
+
+  * Logic is stored in DB, no need to give direct access to all tables.
+
+* Faster Execution
+
+  * Compiled and stored on the DB server.
+
+* Encapsulation
+
+  * Hide complex logic in one callable block.
+
+* Multi-step Operations
+
+  * Perform multiple queries like insert + update + log creation in one procedure.
+
+
+
+
+
+
+
+
+
+
+
+
+===================================================
+Syntax : 
+
+
+CREATE PROCEDURE procedure_name(param1 datatype,
+param2 datatype)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+--  SQL logic here
+END;
+$$;
+
+CALL procedure_name(value1, value2);
