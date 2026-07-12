5d2563fd9ee8e5e89ed5e5e2534d5503d651e8a0
Rohit-coder-py <rohitjha7386@gmail.com>
2026-07-12 21:17:40 +0530

    Day 1 on streamlit

--- 𝗦𝘁𝗿𝗲𝗮𝗺𝗹𝗶𝘁/main.py
+++ 𝗦𝘁𝗿𝗲𝗮𝗺𝗹𝗶𝘁/main.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
 st.title("First Streamlit app")
+
+
+st.title("Hello Chai App")
+st.subheader("Brewed with streamlit")
+st.text("Welcome to your first interative app")
+st.write("Chooose your fav. variety of chai")
+
+chai = st.selectbox("Your fav chai: ", ["Masala chai","Lemon Tea", "Adrak Chai", "Kesar Chai"])
+
+
+st.write("You selected",chai)
+
+
+st.success(f"Your {chai} has been already brewed")
