dee220caa4f5a507e00f70344c7a94d1cdd9f625
Rohit-coder-py <rohitjha7386@gmail.com>
2026-08-11 09:33:27 +0530

    DSA Practice : Time Complexity

--- 𝗗𝗦𝗔/Practice/01_Time Complexity.py
+++ 𝗗𝗦𝗔/Practice/01_Time Complexity.py
@@ -1,15 +1,18 @@
-#simple algorithm
+\#simple algorithm
 
 x = 5
 print(x)
 
-"""
-Time complexity type
-"""
-#1.constant time - O(1)
+# Time complexity type
+
+#1.Constant Time - O(1)            (y = 5)
 
 x = 5
 print(x)
 
+# 2. Linear Time - O(n)     (y = 2x + 5)
 
+n = 5
 
+for i in range(n):
+    print(i)
