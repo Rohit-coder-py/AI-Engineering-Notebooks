168a3b15cd27bfecf67896efdaa0970e0f452c8d
Rohit-coder-py <rohitjha7386@gmail.com>
2026-08-12 13:25:10 +0530

    DSA: Array Completed

--- 𝗗𝗦𝗔/Practice/02_Array in Python.py
+++ 𝗗𝗦𝗔/Practice/02_Array in Python.py
@@ -30,3 +30,50 @@ print('\n')
 #how to know typecode of a arr
     
 print(arr.typecode)
+
+#slincing
+
+arr = array('i', [10, 20, 30, 40, 50, 60])
+
+print("Original Array :", arr)
+
+print("\nIndexing")
+
+print("First Element       :", arr[0])
+
+print("Third Element       :", arr[2])
+
+print("Last Element        :", arr[-1])
+
+print("Second Last Element :", arr[-2])
+
+
+#Slicing
+
+print("arr[:]    :", arr[:])
+print("arr[1:4]  :", arr[1:4])
+print("arr[:3]   :", arr[:3])
+
+print("arr[3:]   :", arr[3:])
+
+print("arr[::2]  :", arr[::2])
+print("arr[::-1] :", arr[::-1])
+arr[1::2]
+
+
+#creating array using numpy module
+
+
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+
+for x in arr:
+    print(x,end = ' ')
+    
+print('\n')
+
+for i in range(len(arr)):
+    print(arr[i],end = ' ')
+    
