2b56ebbecdafb1db41fa242c21aafad2272091a1
Rohit-coder-py <rohitjha7386@gmail.com>
2026-08-14 11:14:31 +0530

    STreamit : Poll booth and image

--- 𝗦𝘁𝗿𝗲𝗮𝗺𝗹𝗶𝘁/chapter_3.py
+++ 𝗦𝘁𝗿𝗲𝗮𝗺𝗹𝗶𝘁/chapter_3.py
@@ -3,7 +3,6 @@ import streamlit as st
 #goal : To learn how to learn layouts using streamlit
 st.header("Hello")
 st.title("Chai Taste Poll")
-
 col_1 , col_2 = st.columns(2)
 
 with col_1:
@@ -12,6 +11,9 @@ with col_1:
 with col_2:
     st.header("Milk Chai")
     vote2 = st.button("Vote for Milk Chai ")
+    st.image(r"C:\Users\shobh\OneDrive\Attachments\Pictures\Screenshots 1\Screenshot 2026-08-14 110615.png",width = 600)
     
 if vote1:
-    print("shio")
+    st.success("Thanks for voting Masala Chai")
+if vote2:
+    st.success("Thanks for voting Milk Chai")
